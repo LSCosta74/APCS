c373de6643fed6e2067c12172f74b2f8c688bf55
Lucas Costa <lucascosta@ORRHS.ORG>
2020-02-12 11:02:16 -0500

    Main Page with Button

---
+++ Clicker Game/main.js
@@ -0,0 +1,13 @@
+let button;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+      button = createButton('GO TO BANANA GAME');
+      button.position(width/2, height/2)
+      button.mousePressed(goToLink)
+      background(220);
+}
+
+function goToLink() {
+    window.location.href = 'game.html'; 
+}
