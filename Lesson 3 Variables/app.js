764d2be7e0c58683a24fea3fb07de3aa85776b19
Lucas Costa <lucascosta@ORRHS.ORG>
2020-02-24 11:49:18 -0500

    Challenges with Variables

--- Lesson 3 Variables/app.js
+++ Lesson 3 Variables/app.js
@@ -27,3 +27,38 @@
 // console.log("and a: " + roll2) 
 // let totalDice = roll1 + roll2
 // console.log("For a total of + totalDice)
+
+//VARIABLE REASSIGNMENT
+// let a = 3
+// let b = 7
+// a = b
+// b = a
+// console.log(b)
+// console.log(a)
+
+// let  someNum = 7;
+// someNum = someNum + 1;
+// console.log("The value of someNum is now: " + someNum);
+// someNum = someNum + 1;
+// console.log("The value of someNum is now: " + someNum);
+// someNum = someNum + 1;
+// console.log("The value of someNum is now: " + someNum);
+// someNum = someNum * someNum;
+// console.log("The final value is now: " + someNum)
+
+// CHALLENGE 2
+// let x = 7;
+// let y = 3;
+// y=x-y
+// console.log("The value of y is: " + y + " (y should be 4)");
+
+// CHALLENGE 3
+// let x = 2;
+// let y = 3;
+// x = x + x + y
+// console.log("The value of x is: " + x + " (x should be 7)")
+
+//CHALLENGE 4
+// let x = 2
+// x=x+x+x+x
+// console.log("The value of x is: " + x + " (x should be 8)")
